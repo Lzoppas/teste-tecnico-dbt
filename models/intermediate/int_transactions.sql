edebca647a5b3a93ab93a6f79bf337b1d08947ca
Lucas Zoppas <lucasmzoppas@gmail.com>
2026-05-14 23:58:28 -0300

    Refatora signed_amount usando macro signed_value

--- models/intermediate/int_transactions.sql
+++ models/intermediate/int_transactions.sql
@@ -9,10 +9,7 @@ select
     t.transaction_date,
     t.quantity * t.unit_price as gross_amount,
     {{ signed_value('t.operation', 't.quantity') }} as signed_quantity,
-    case
-        when t.operation = 'BUY' then t.quantity * t.unit_price
-        when t.operation = 'SELL' then -(t.quantity * t.unit_price)
-    end as signed_amount
+    {{ signed_value('t.operation', 't.quantity * t.unit_price') }} as signed_amount
 from {{ ref('stg_transactions') }} t
 left join {{ ref('stg_clients') }} c
     on t.client_id = c.client_id
